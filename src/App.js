efcbf2ff8b5a5e7bb12bcfbca1a1ee2427ef3af5
ledungcobra <ledungcobra@gmail.com>
2020-09-08 17:48:49 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
+
+const HatsPage = (props) => {
+  console.log(props);
+  return <div>
+    <h1>Hats page</h1>
+  </div>
+}
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch >
+        <Route exact path='/'  component={HomePage} />
+        <Route exact path='/shop'  component={ShopPage} />
+      </Switch>
     </div>
   );
 }
